0acab3bdfe51b262f63f0dec5c0fb47cd334b92f
Brian McClain <brianmmcclain@gmail.com>
2018-08-22 15:50:58 -0400

    Update header

--- app.rb
+++ app.rb
@@ -6,5 +6,5 @@ get '/' do
   port = ENV['CF_INSTANCE_PORT'] || "9292"
   index = ENV['CF_INSTANCE_INDEX'] || "0"
 
-  "<h1>Ruby Demo</h1>Host: #{host}:#{port}<br />Index: #{index}"
+  "<h1>Ruby Concourse Demo</h1>Host: #{host}:#{port}<br />Index: #{index}"
 end
